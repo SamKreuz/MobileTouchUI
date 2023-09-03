f7b3e0a941f2f514155494feb086b173681e760d
Samuel Kreuz <sam@samuelkreuz.com>
2023-09-03 20:56:29 +0200

    Connection lines and animation added

---
+++ Assets/Scripts/Line.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    internal class Line
+    {
+
+        public GameObject GameObject { get; }
+        public GameObject PointA { get; }
+        public GameObject PointB { get; }
+
+        private LineRenderer lineRenderer;
+
+        public Line(GameObject gameObject, GameObject pointA, GameObject pointB) 
+        { 
+            GameObject = gameObject;
+            PointA = pointA;
+            PointB = pointB;
+            lineRenderer = gameObject.GetComponent<LineRenderer>();
+            Debug.Log($"LineRenderer for {gameObject.name} created");
+        }
+
+        public bool ContainsPoint(string point)
+        {
+            return PointA.name == point || PointB.name == point;
+        }
+
+        public void Remove()
+        {
+            UnityEngine.Object.Destroy(GameObject);
+        }
+
+        public void UpdatePosition() 
+        {
+            lineRenderer.SetPosition(0, PointA.transform.position);
+            lineRenderer.SetPosition(1, PointB.transform.position);
+        }
+    }
+}
--- Assets/Scripts/ManageInputs.cs
+++ Assets/Scripts/ManageInputs.cs
@@ -1,16 +1,19 @@
+using Assets.Scripts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using EnhancedTouch = UnityEngine.InputSystem.EnhancedTouch;
 
 public class ManageInputs : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject graphic;
+    [SerializeField] private GameObject graphic;
+    [SerializeField] private Material lineMaterial;
+
+    private float lineWidth = 0.1f;
     private Dictionary<int, GameObject> circles = new();
-    private LineRenderer lineRenderer;
+    private List<Line> linesList = new();
+    private Animator animator;
 
     protected void OnEnable()
     {
@@ -18,9 +21,6 @@ public class ManageInputs : MonoBehaviour
         EnhancedTouch.Touch.onFingerDown += Touch_onFingerDown;
         EnhancedTouch.Touch.onFingerMove += Touch_onFingerMove;
         EnhancedTouch.Touch.onFingerUp += Touch_onFingerUp;
-
-        //lineRenderer = new LineRenderer();
-        lineRenderer = this.GetComponent<LineRenderer>();
     }
 
     protected void OnDisable()
@@ -33,23 +33,26 @@ public class ManageInputs : MonoBehaviour
 
     public void Update()
     {
-        if(circles.Count >= 2) 
-        {
-            var pos1 = circles[0].transform.position;
-            var pos2 = circles[1].transform.position;
-            lineRenderer.SetPosition(0, pos1);
-            lineRenderer.SetPosition(1, pos2);
-            Debug.Log("Positions: " + pos1 + ", " + pos2);
-        }
+        // TODO SK: Delete when not needed anymore
     }
 
     private void Touch_onFingerDown(EnhancedTouch.Finger finger)
     {
         Debug.Log("New Finger Down: " + finger.index);
+        Debug.Log("Active Lines: " + linesList.Count());
 
         var newCircle = Instantiate(graphic, GetScreenPosition(finger), Quaternion.identity);
 
+        Guid newCircleId = Guid.NewGuid();
+        newCircle.name = newCircleId.ToString();
+
+        animator = newCircle.GetComponent<Animator>();
+        animator.Play("CircleScaleUp");
+
+        CreateLines(newCircle);
+
         circles.Add(finger.index, newCircle);
+        //circlesList.Add(new Circle(finger.index, newCircle.name, newCircle));
     }
 
     /// <summary>
@@ -58,18 +61,28 @@ public class ManageInputs : MonoBehaviour
     /// <param name="finger"></param>
     private void Touch_onFingerMove(EnhancedTouch.Finger finger)
     {
-        Debug.Log("Finger moved: " + finger.index);
-
         var currentCircle = circles[finger.index];
 
         currentCircle.transform.position = GetScreenPosition(finger);
+
+        Debug.Log("Finger moved: " + finger.index + ", Name: " + currentCircle.name);
+
+        linesList.Where(x => x.ContainsPoint(currentCircle.name)).ToList().ForEach(x => x.UpdatePosition());
     }
 
     private void Touch_onFingerUp(EnhancedTouch.Finger finger)
     {
         var circleToRemove = circles[finger.index];
+
+        Debug.Log("Finger removed: " + finger.index + ", Name: " + circleToRemove.name);
+
+        var linesToRemove = linesList.Where(x => x.ContainsPoint(circleToRemove.name)).ToList();
+        linesToRemove.ForEach(x => x.Remove());
+        linesList.RemoveAll(x => x.ContainsPoint(circleToRemove.name));
+
         Destroy(circleToRemove);
         circles.Remove(finger.index);
+        //circlesList.RemoveAll(x => x.Index == finger.index);
 
         Debug.Log($"Circles left: {circles.Count}");
     }
@@ -81,5 +94,33 @@ public class ManageInputs : MonoBehaviour
         fingerWorldPosition.z = 0;
 
         return fingerWorldPosition;
+    }
+
+    private void CreateLines(GameObject newCircle)
+    {
+        // for each finger/circle create a new line to this newly created one
+        foreach (var circle in circles.Values)
+        {
+            var pos1 = newCircle.transform.position;
+            var pos2 = circle.transform.position;
+
+            GameObject lineObject = new GameObject(Guid.NewGuid().ToString());
+
+            LineRenderer lineRenderer = lineObject.AddComponent<LineRenderer>();
+            if (lineRenderer != null)
+            {
+                lineRenderer.SetPosition(0, pos1);
+                lineRenderer.SetPosition(1, pos2);
+                lineRenderer.startWidth = lineWidth;
+                lineRenderer.endWidth = lineWidth;
+                lineRenderer.material = lineMaterial;
+            }
+            else
+            {
+                Debug.LogWarning("LineRenderer could not be created.");
+            }
+
+            linesList.Add(new Line(lineObject, newCircle, circle));
+        }
     }
 }
